b786fe1eabc8cfcc582a9c4fbf0672d49f3cebab
vaporstack <andrew@vaporstack.org>
2018-05-16 15:38:44 -0400

    Untangling from the rest of the ecosystem, slowly

---
+++ src/ext/drw_ext_gpc.c
@@ -0,0 +1,9 @@
+//
+//  drw_ext_gpc.c
+//  drw
+//
+//  Created by vs on 5/16/18.
+//  Copyright © 2018 ruminant. All rights reserved.
+//
+
+#include "drw_ext_gpc.h"
---
+++ src/ext/drw_ext_gpc.h
@@ -0,0 +1,14 @@
+//
+//  drw_ext_gpc.h
+//  drw
+//
+//  Created by vs on 5/16/18.
+//  Copyright © 2018 ruminant. All rights reserved.
+//
+
+#ifndef drw_ext_gpc_h
+#define drw_ext_gpc_h
+
+#include <stdio.h>
+
+#endif /* drw_ext_gpc_h */
